94e9e03a7b42aa5185588819456cb88325c59142
finnus <40977905+finnus@users.noreply.github.com>
2023-11-21 14:30:56 +0100

    use current mapengine, fix typo

--- slapp/explorer/urls.py
+++ slapp/explorer/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-app_name = "users"
+app_name = "explorer"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/map.html"), name="map"),
     path("details/", TemplateView.as_view(template_name="pages/details.html"), name="details"),
