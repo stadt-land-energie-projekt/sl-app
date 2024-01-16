adf9363b933670f4c83c52885301101fc4f6e214
Finn <40977905+finnus@users.noreply.github.com>
2024-01-16 11:15:10 +0100

    add detail view basic filter (#28)

--- slapp/explorer/urls.py
+++ slapp/explorer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from slapp.explorer import views
+
 app_name = "explorer"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/map.html"), name="map"),
-    path("details/", TemplateView.as_view(template_name="pages/details.html"), name="details"),
+    path("details/", views.MunicipalityListView.as_view(), name="details"),
 ]
