bc796cd6cccb49fb1e13c7ff39770a6edc8cd61c
finnus <40977905+finnus@users.noreply.github.com>
2024-02-08 09:29:53 +0100

    small fixes for urls and counts

--- slapp/explorer/urls.py
+++ slapp/explorer/urls.py
@@ -1,7 +1,6 @@
 """Urls for explorer app."""
 
 from django.urls import path
-from django.views.generic import TemplateView
 
 from slapp.explorer import views
 
@@ -9,7 +8,7 @@ app_name = "explorer"
 urlpatterns = [
     path("", views.MapGLView.as_view(), name="map"),
     path("details/", views.details_list, name="details"),
-    path("parameters/", TemplateView.as_view(template_name="pages/parameters.html"), name="parameters"),
+    path("parameters/", views.optimization_parameters, name="parameters"),
     path("details/csv/", views.details_csv, name="details-csv"),
 ]
 
