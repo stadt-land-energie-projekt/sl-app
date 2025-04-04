c3171d80220c533c5abce82fb9608ef00a051216
Bryan Lancien <bryanlancien.ui@gmail.com>
2025-04-04 11:14:00 +0100

    finish styling results page

--- slapp/explorer/regions.py
+++ slapp/explorer/regions.py
@@ -560,7 +560,7 @@ def get_dataframes() -> tuple[pd.DataFrame, pd.DataFrame, float]:
 def build_table_data(dataframe: pd.DataFrame, scale: float) -> list[dict]:
     """Convert the given DataFrame into a list of dictionaries for templating."""
     # Fixed colors for first three rows
-    fixed_colors = ["#cdf4d3", "#ffe0c2", "#ffcdc2"]
+    fixed_colors = ["#FFED6F", "#FFCA89", "#FE9797"]
 
     # Reset index so we have a column "index"
     dataframe = dataframe.reset_index()
